4232596b0829eaa5d44b9465da84be20076ea77e
Jean Pondy <manbayesse@gmail.com>
2024-08-30 08:10:13 +0200

    Richtige Karte anzeigen

--- ringOfFireJP/src/app/game/game.component.ts
+++ ringOfFireJP/src/app/game/game.component.ts
@@ -12,6 +12,7 @@ import { Game } from '../../models/game';
 })
 export class GameComponent {
   takeCardAnimation = false;
+  currentCard: string = '';
   game: Game;
 
 
@@ -28,12 +29,27 @@ console.log(this.game)
 }
 
 
-takeCard(){
+takeCard() {
+  /* nur this takeCardAnimation false ist denn wird alles aus geführt */
+  if (!this.takeCardAnimation){ 
+  this.currentCard = this.game.stack.pop()!;
+  console.log(this.currentCard)
   this.takeCardAnimation = true;
+ 
+  console.log('New card:' + this.currentCard);
+  console.log('Game is', this.game);
+
+  /* Nächsten Spieler auswählen */
+  this.game.currentPlayer++;
+  this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+
+/* die Animation nach 1500s wiederholen */
+  setTimeout(()  => {
+    this.game.playedCards.push(this.currentCard);
+    this.takeCardAnimation = false;
+  }, 1000); // muss gleiche Zeit wie die Animation sein
+}
 }
 
-
-
-
-  
 }
