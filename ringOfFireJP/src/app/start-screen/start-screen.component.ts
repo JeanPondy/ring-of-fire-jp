badcb415d01c2e65e3928f68a5c0ca8655657c9c
Jean Pondy <manbayesse@gmail.com>
2024-08-30 06:36:00 +0200

    game

--- ringOfFireJP/src/app/start-screen/start-screen.component.ts
+++ ringOfFireJP/src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';   // start game 1
 
 @Component({
   selector: 'app-start-screen',
@@ -9,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+  constructor(private router: Router) { }   // start game 2
+
+  newGame() {
+    // start game 3
+    this.router.navigateByUrl('/game');
+  }
 }
